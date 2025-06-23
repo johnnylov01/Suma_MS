5d87ebbb3c8c1e6d85233de4f5082b79ccc63f6c
Johnnylov01 <juaninflamas@gmail.com>
2025-06-23 05:30:25 -0600

    aa

--- suma_service.py
+++ suma_service.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -22,4 +23,5 @@ def sumar_endpoint():
         return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(port=5001)
+    port = int(os.environ.get('PORT', 80))
+    app.run(host='0.0.0.0', port=port)
